958d88d89d5b919af9fd7636175483fe7274310f
cuong982 <luongvancuongkmhd1998@gmail.com>
2020-05-06 21:40:40 +0700

    rbi6

--- RbiCloud/settings.py
+++ RbiCloud/settings.py
@@ -125,7 +125,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-Static files (CSS, JavaScript, Images)
+#Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
