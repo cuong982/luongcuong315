ff6c8ab95a9f3145d15d81e3f80f67eb9002af43
cuong982 <luongvancuongkmhd1998@gmail.com>
2020-05-07 15:35:33 +0700

    rbi11

--- cloud/views.py
+++ cloud/views.py
@@ -3393,10 +3393,10 @@ def signin(request):
                         return redirect('manager',3)
                 else:
                     error="Tài khoản hoặc mật khẩu không đúng"
-        return render(request,'Home/index.html',{'error':error})
     except Exception as e:
         print(e)
         # raise Http404
+    return render(request, 'Home/index.html', {'error': error})
 def logout(request):
     try:
         del request.session['id']
