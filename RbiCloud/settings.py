961ed96deeb23ff0f4fa14b30e8515a63978980a
cuong982 <luongvancuongkmhd1998@gmail.com>
2020-05-07 23:51:40 +0700

    rbi

--- RbiCloud/settings.py
+++ RbiCloud/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
+django_heroku.setting(locals())
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
