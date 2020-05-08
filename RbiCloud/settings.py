e1aec505dc506dccb6fcce10f5d572a8c58a38b8
cuong982 <luongvancuongkmhd1998@gmail.com>
2020-05-08 15:30:14 +0700

    rbi

--- RbiCloud/settings.py
+++ RbiCloud/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    #'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.sessions.backends.cached_db',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
